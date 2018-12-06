9d254e4c21e8679e4361d4d9063d0d480d518e1c
Polaris00418 <qinglong0418@gmail.com>
2018-12-06 09:34:58 +0800

    Update widget.js

--- demos/appearance/widget.js
+++ demos/appearance/widget.js
@@ -1,7 +1,7 @@
 //ref : https://github.com/blog/273-github-ribbons
 document.addEventListener("DOMContentLoaded", function(event) { 
     var githubRibbon=document.createElement('a');
-    githubRibbon.setAttribute('href', 'https://github.com/bruce0418/jeelizFaceFilter');
+    githubRibbon.setAttribute('href', '');
     var githubRibbonImage=document.createElement('img');
     githubRibbonImage.setAttribute('style', 'position: absolute; top: 0; left: 0; border: 0; z-index: 1000');
     githubRibbonImage.setAttribute('src', ' ');
